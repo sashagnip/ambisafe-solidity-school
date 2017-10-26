9e5c7120eec3f80768ec8494ea2cf9445360cc3a
qwerty <qwerty@qwertys-MacBook-Pro.local>
2017-10-26 23:30:50 +0300

    [+] events [*] zero amount fix [*] style fix

--- borrow.sol
+++ borrow.sol
@@ -6,22 +6,45 @@ contract Borrow {
         uint amount;
     }
 
-    uint lastDebtId;
     mapping(uint => Debt) debts;
+    uint lastDebtId;
 
-    function createDebt(address _to, uint _amount) public returns (uint thisId) {
-        thisId = lastDebtId++;
-        debts[thisId] = Debt(msg.sender, _to, _amount);
+    event CreatedDebt(uint id, 
+                      address from, 
+                      address to, 
+                      uint amount);
+
+    event DecreasedDebt(uint id, 
+                        address from, 
+                        address to, 
+                        uint amount, 
+                        uint left);
+
+    function createDebt(address to, uint amount) public returns (uint) {
+        if (amount == 0) revert();
+        uint id = lastDebtId++;
+        debts[id] = Debt(msg.sender, to, amount);
+        CreatedDebt(id, 
+                    msg.sender, 
+                    to,
+                    amount);
+        return id;
     }
     
-    function decDebt(uint _id, uint _amount) public {
-        if (_id > lastDebtId) revert();
-        if (debts[_id].to != msg.sender) revert();
-        if (_amount > debts[_id].amount) revert();
-        debts[_id].amount -= _amount;
+    function decDebt(uint id, uint amount) public {
+        if (amount == 0) revert();
+        if (id > lastDebtId) revert();
+        if (debts[id].to != msg.sender) revert();
+        if (amount > debts[id].amount) revert();
+        debts[id].amount -= amount;
+        DecreasedDebt(id, 
+                      debts[id].from, 
+                      debts[id].to, 
+                      amount, 
+                      debts[id].amount);
     }
-    
-    function showDebt(uint _id) public view returns (address, address, uint) {
-        return (debts[_id].from, debts[_id].to, debts[_id].amount);
+
+    function showDebt(uint id) public view returns (address, address, uint) {
+        return (debts[id].from, debts[id].to, debts[id].amount);
     }
 }
